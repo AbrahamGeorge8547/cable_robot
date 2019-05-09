36fa4cfab8f0b883a13d7364349e97eb53034f80
aromal <aromal@pop-os.localdomain>
2019-05-09 22:46:06 +0530

    test_driver

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cable_robot
 A different type of cable robot
+the red-blue pair gives access to one coil and the black-green pair gives access to the other coil
+SHORT RESET AND SLEEP
---
+++ start.py
@@ -0,0 +1,19 @@
+# import math
+# import stepper
+# import threading
+# d = float(input('D value: '))
+# r = float(input('R value: '))
+# L1 = (5 * d)/(2 * math.pi * r)
+# L2 = (math.sqrt(50) * d)/(2 * math.pi * r)
+# N1 = ((6 - L1) * d)/(2 * math.pi * r)
+# N2 = ((6 - L2) * d)/(2 * math.pi * r)
+# while True: 
+#     x = int(input('X value: '))
+#     y = int(input('Y value: '))
+#     l1 = math.sqrt(stepper.square(x) + stepper.square(y-5))
+#     l2 = math.sqrt(stepper.square(5-x) + stepper.square(y-5))
+#     n1 = ((6 - l1) * d)/(2 * math.pi * r)
+#     n2 = ((6 - l2) * d)/(2 * math.pi * r)
+#     if (N1-n1>0) {
+        
+#     }
