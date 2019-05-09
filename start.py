95fd7c6bab26dadaa32996e0edbae223c29e707d
aromal <aromal@pop-os.localdomain>
2019-05-09 23:39:10 +0530

    for firstmotot

--- start.py
+++ start.py
@@ -1,19 +1,27 @@
-# import math
-# import stepper
-# import threading
-# d = float(input('D value: '))
-# r = float(input('R value: '))
-# L1 = (5 * d)/(2 * math.pi * r)
-# L2 = (math.sqrt(50) * d)/(2 * math.pi * r)
-# N1 = ((6 - L1) * d)/(2 * math.pi * r)
-# N2 = ((6 - L2) * d)/(2 * math.pi * r)
-# while True: 
-#     x = int(input('X value: '))
-#     y = int(input('Y value: '))
-#     l1 = math.sqrt(stepper.square(x) + stepper.square(y-5))
-#     l2 = math.sqrt(stepper.square(5-x) + stepper.square(y-5))
-#     n1 = ((6 - l1) * d)/(2 * math.pi * r)
-#     n2 = ((6 - l2) * d)/(2 * math.pi * r)
-#     if (N1-n1>0) {
-        
-#     }
+import math
+import stepper
+import threading
+
+def square(x):
+    return x*x
+d = float(input('D value: '))
+r = float(input('R value: '))
+L1 = (5 * d)/(2 * math.pi * r)
+L2 = (math.sqrt(50) * d)/(2 * math.pi * r)
+N1 = ((6 - L1) * d)/(2 * math.pi * r)
+N2 = ((6 - L2) * d)/(2 * math.pi * r)
+while True: 
+    x = int(input('X value: '))
+    y = int(input('Y value: '))
+    l1 = math.sqrt(square(x) + square(y-5))
+    l2 = math.sqrt(square(5-x) + square(y-5))
+    n1 = ((6 - l1) * d)/(2 * math.pi * r)
+    n2 = ((6 - l2) * d)/(2 * math.pi * r)
+    n1_diff = N1-n1
+    n2_diff = N2-n2
+    if N1-n1>0 :
+        t1 = threading.Thread(target=stepper.clockwise, args=(n1_diff,))
+        t1.start()
+    else: 
+        t1 = threading.Thread(target=stepper.counterclockwise, args=(n1_diff,))
+        t1.start()
--- test_driver.py
+++ test_driver.py
@@ -10,7 +10,6 @@ SPR = 200       # Steps per Revolution (360 / 1.8)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
-GPIO.output(DIR, CW)
 
 MODE = (14, 15, 18) # Microstep Resolution GPIO Pins
 GPIO.setup(MODE, GPIO.OUT)
@@ -23,20 +22,21 @@ RESOLUTION = {'Full': (0, 0, 0),
 
 GPIO.output(MODE, RESOLUTION['1/32'])
 
-step_count = SPR * 4
 delay = .005 / 32
-
-for x in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)
-    sleep(delay)
-    GPIO.output(STEP, GPIO.LOW)
-    sleep(delay)
+def clockwise(step_count):
+    GPIO.output(DIR, CW)
+    for x in range(step_count):
+        GPIO.output(STEP, GPIO.HIGH)
+        sleep(delay)
+        GPIO.output(STEP, GPIO.LOW)
+        sleep(delay)
 
 sleep(.5)
-GPIO.output(DIR, CCW)
 
-for x in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)
-    sleep(delay)
-    GPIO.output(STEP, GPIO.LOW)
-sleep(delay)
+def Counterclockwise(step_count):
+    GPIO.output(DIR, CCW)
+    for x in range(step_count):
+        GPIO.output(STEP, GPIO.HIGH)
+        sleep(delay)
+        GPIO.output(STEP, GPIO.LOW)
+        sleep(delay)
