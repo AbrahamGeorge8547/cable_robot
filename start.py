fba84af47541f6b73f50895793e7183f068f8253
abrahamgeorge8547 <abrahamgeorge8547@gmail.com>
2019-06-16 16:13:28 +0530

    m2_add

--- start.py
+++ start.py
@@ -1,6 +1,10 @@
 import math
 import stepper
 import threading
+DIR_M1 = 5       # Direction GPIO Pin
+STEP_M1 = 6      # Step GPIO Pin
+DIR_M2 = 13       # Direction GPIO Pin
+STEP_M2 = 19      # Step GPIO Pin
 
 def square(x):
     return x*x
@@ -19,9 +23,37 @@ while True:
     n2 = ((6 - l2) * d)/(2 * math.pi * r)
     n1_diff = N1-n1
     n2_diff = N2-n2
+    m1_dir = True
+    m2_dir = True
     if N1-n1>0 :
-        t1 = threading.Thread(target=stepper.clockwise, args=(n1_diff,))
+        t1 = threading.Thread(target=stepper.clockwise, args=(n1_diff, DIR_M1, STEP_M1, ))
         t1.start()
     else: 
-        t1 = threading.Thread(target=stepper.counterclockwise, args=(n1_diff,))
+        t1 = threading.Thread(target=stepper.counterclockwise, args=(n1_diff, DIR_M1, STEP_M1, ))
+        m1_dir = False
         t1.start()
+    if N2-n2>0 :
+        t2 = threading.Thread(target=stepper.clockwise, args=(n2_diff, DIR_M2, STEP_M2, ))
+        t2.start()
+    else: 
+        t2 = threading.Thread(target=stepper.counterclockwise, args=(n2_diff, DIR_M2, STEP_M2, ))
+        m2_dir = False
+        t2.start()
+    t1.join()
+    t2.join()
+    time.sleep(8)
+    if(m1_dir) :
+        t1 = threading.Thread(target=stepper.counterclockwise, args=(n1_diff, DIR_M1, STEP_M1, ))
+        t1.start()
+    else:
+        t1 = threading.Thread(target=stepper.clockwise, args=(n1_diff, DIR_M1, STEP_M1, ))
+        t1.start()
+    if(m2_dir) :
+        t2 = threading.Thread(target=stepper.counterclockwise, args=(n2_diff, DIR_M1, STEP_M1, ))
+        t2.start()
+    else:
+        t2 = threading.Thread(target=stepper.clockwise, args=(n2_diff, DIR_M1, STEP_M1, ))
+        t2.start()
+    time.sleep(8)
+
+
--- stepper.py
+++ stepper.py
@@ -2,18 +2,23 @@
 from time import sleep
 import RPi.GPIO as GPIO
 
-DIR = 20       # Direction GPIO Pin
-STEP = 21      # Step GPIO Pin
+DIR_M1 = 5       # Direction GPIO Pin
+STEP_M1 = 6      # Step GPIO Pin
+DIR_M2 = 13       # Direction GPIO Pin
+STEP_M2 = 19      # Step GPIO Pin
 CW = 1         # Clockwise Rotation
 CCW = 0        # Counterclockwise Rotation
 SPR = 200       # Steps per Revolution (360 / 1.8)
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(DIR, GPIO.OUT)
-GPIO.setup(STEP, GPIO.OUT)
-
-MODE = (14, 15, 18) # Microstep Resolution GPIO Pins
-GPIO.setup(MODE, GPIO.OUT)
+GPIO.setup(DIR_M1, GPIO.OUT)
+GPIO.setup(STEP_M1, GPIO.OUT)
+GPIO.setup(DIR_M2, GPIO.OUT)
+GPIO.setup(STEP_M2, GPIO.OUT)
+MODE_M1 = (14, 15, 18) # Microstep Resolution GPIO Pins for M1
+MODE_M2 = (16, 20, 21) # Microstep Resolution GPIO pins for M2
+GPIO.setup(MODE_M1, GPIO.OUT)
+GPIO.setup(MODE_M2, GPIO.OUT)
 RESOLUTION = {'Full': (0, 0, 0),
               'Half': (1, 0, 0),
               '1/4': (0, 1, 0),
@@ -21,10 +26,10 @@ RESOLUTION = {'Full': (0, 0, 0),
               '1/16': (0, 0, 1),
               '1/32': (1, 0, 1)}
 
-GPIO.output(MODE, RESOLUTION['1/32'])
-
+GPIO.output(MODE_M1, RESOLUTION['1/32'])
+GPIO.output(MODE_M2, RESOLUTION['1/32'])
 delay = .005 / 32
-def clockwise(step_count,):
+def clockwise(step_count, DIR, STEP, ):
     step_count = SPR * step_count
     GPIO.output(DIR, CW)
     for x in range(step_count):
@@ -33,9 +38,8 @@ def clockwise(step_count,):
         GPIO.output(STEP, GPIO.LOW)
         sleep(delay)
 
-sleep(.5)
 
-def counterclockwise(step_count,):
+def counterclockwise(step_count, DIR, STEP, ):
     step_count = SPR * step_count
     GPIO.output(DIR, CCW)
     for x in range(step_count):
